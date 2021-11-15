b40af41d92ceb5a9edfd1a23b9165f7cefa076b4
Steve Heindel <stevenheindel@gmail.com>
2021-11-14 21:15:29 -0500

    Fix Floor1 decoding

--- src/floor.rs
+++ src/floor.rs
@@ -65,7 +65,7 @@ impl Floor0 {
 pub struct Floor1 {
     partitions: u8,
     partition_class_list: Vec<u8>,
-    maximum_class: Option<u8>,
+    maximum_class: u8,
     classes: Vec<Class>,
     multiplier: u8,
     rangebits: u8,
@@ -81,48 +81,38 @@ impl Floor1 {
         let partitions = reader.read(5).unwrap();
         let partition_class_list: Vec<u8> =
             (0..partitions).map(|_| reader.read(4).unwrap()).collect();
-        let maximum_class = partition_class_list.iter().copied().max();
+        let maximum_class = partition_class_list.iter().copied().max().unwrap();
 
         let mut classes: Vec<Class> = Vec::new();
-        if let Some(max) = maximum_class {
-            for _ in 0..max {
-                let dimensions = reader.read::<u8>(3).unwrap() + 1;
-                let subclasses = reader.read(2).unwrap();
-                let masterbooks = if subclasses > 0 {
-                    // TODO: validate that this element is not greater than the highest numbered codebook
-                    Some(reader.read(8).unwrap())
-                } else {
-                    None
-                };
-                let max = u8::pow(2, subclasses as u32);
-                let subclass_books: Vec<u8> = (0..max)
-                    .map(|_| {
-                        let val: u8 = reader.read(8).unwrap();
-                        assert_ne!(val, 0);
-                        // TODO: validate that this element is not greater than the highest numbered codebook
-                        val - 1
-                    })
-                    .collect();
+        for _ in 0..=maximum_class {
+            let dimensions = reader.read::<u8>(3).unwrap() + 1;
+            let subclasses = reader.read(2).unwrap();
+            let masterbooks = if subclasses > 0 {
+                // TODO: validate that this element is not greater than the highest numbered codebook
+                Some(reader.read(8).unwrap())
+            } else {
+                None
+            };
+            let max = 2_u8.pow(subclasses as u32);
+            let subclass_books: Vec<i32> = (0..max)
+                .map(|_| reader.read::<i32>(8).unwrap() - 1) // TODO: validate that this element is not greater than the highest numbered codebook
+                .collect(); // TODO: spec says this is an unsigned integer; but what to do if its value is zero before subtracting? Treat as -1 or wrap to 0xFF (or 0xFFFFFFFF)?
 
-                classes.push(Class {
-                    dimensions,
-                    subclasses,
-                    masterbooks,
-                    subclass_books,
-                });
-            }
+            classes.push(Class {
+                dimensions,
+                subclasses,
+                masterbooks,
+                subclass_books,
+            });
         }
 
         let multiplier = reader.read::<u8>(2).unwrap() + 1;
         let rangebits = reader.read(4).unwrap();
-        let mut x_list: Vec<u32> = Vec::new();
-        x_list[0] = 0;
-        x_list[1] = u32::pow(2, rangebits as u32);
-        assert_ne!(partitions, 0);
+        let mut x_list: Vec<u32> = vec![0, 2u32.pow(rangebits as u32)];
         for &current_class_number in &partition_class_list {
             let max = classes[current_class_number as usize].dimensions;
             assert_ne!(max, 0);
-            x_list.extend((0..=max).map(|_| reader.read::<u32>(rangebits as u32).unwrap()));
+            x_list.extend((0..max).map(|_| reader.read::<u32>(rangebits as u32).unwrap()));
         }
         assert!(x_list.len() <= 65);
         // TODO: validate that all element values in x_list are unique within the vector
@@ -144,7 +134,7 @@ pub struct Class {
     dimensions: u8,
     subclasses: u8,
     masterbooks: Option<u8>,
-    subclass_books: Vec<u8>,
+    subclass_books: Vec<i32>,
 }
 
 #[cfg(test)]

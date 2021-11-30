90b29137e04a76b7c01e03492dab08214ab6f3b0
Steve Heindel <stevenheindel@gmail.com>
2021-11-29 23:06:39 -0500

    Tests for floor

--- src/floor.rs
+++ src/floor.rs
@@ -1,7 +1,7 @@
 use bitstream_io::{BitRead, BitReader};
 use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Floor {
     Zero(Floor0),
     One(Floor1),
@@ -23,7 +23,7 @@ impl Floor {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct Floor0 {
     order: u8,
     rate: u16,
@@ -63,7 +63,7 @@ impl Floor0 {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct Floor1 {
     partitions: u8,
     partition_class_list: Vec<u8>,
@@ -114,9 +114,7 @@ impl Floor1 {
         let mut x_list: Vec<u32> = vec![0, 2u32.pow(rangebits as u32)];
         for &current_class_number in &partition_class_list {
             let max = classes[current_class_number as usize].dimensions;
-            if max == 0 {
-                return Err(FloorError::ZeroDimensions(current_class_number));
-            }
+            assert_ne!(max, 0); // Should be impossible by design; dimensions is always a 3 bit number + 1
             for _ in 0..max {
                 let val = reader.read::<u32>(rangebits as u32)?;
                 x_list.push(val);
@@ -140,7 +138,7 @@ impl Floor1 {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct Class {
     dimensions: u8,
     subclasses: u8,
@@ -152,9 +150,6 @@ pub struct Class {
 pub enum FloorError {
     #[error("Invalid floor type: {0}")]
     InvalidFloorType(u16),
-
-    #[error("Zero dimensions found in class number {0}")]
-    ZeroDimensions(u8),
 
     #[error("Floor X list too long: {0}")]
     XListTooLong(usize),
@@ -169,5 +164,78 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_() {}
+    fn test_floor_decode() {
+        use bitstream_io::{BitReader, LittleEndian};
+        use std::io::Cursor;
+
+        // Frampton floor config 0 (type 1)
+        let input = [
+            1, 0, 6, 34, 100, 38, 16, 40, 128, 2, 3, 25, 0, 112, 128, 144, 32, 5, 0, 20, 22, 24,
+            58, 134, 139, 128, 128, 92, 66, 70, 129, 65, 225, 152, 112, 78, 58, 109,
+        ];
+        let mut cursor = Cursor::new(input);
+        let mut reader = BitReader::endian(&mut cursor, LittleEndian);
+        let floor = Floor::decode(&mut reader).unwrap();
+        assert_eq!(
+            floor,
+            Floor::One(Floor1 {
+                partitions: 6,
+                partition_class_list: vec![0, 1, 1, 2, 3, 3],
+                maximum_class: 3,
+                classes: vec![
+                    Class {
+                        dimensions: 2,
+                        subclasses: 0,
+                        masterbooks: None,
+                        subclass_books: vec![3],
+                    },
+                    Class {
+                        dimensions: 3,
+                        subclasses: 1,
+                        masterbooks: Some(0),
+                        subclass_books: vec![4, 5],
+                    },
+                    Class {
+                        dimensions: 3,
+                        subclasses: 2,
+                        masterbooks: Some(1),
+                        subclass_books: vec![-1, 6, 7, 8],
+                    },
+                    Class {
+                        dimensions: 3,
+                        subclasses: 2,
+                        masterbooks: Some(2),
+                        subclass_books: vec![-1, 9, 10, 11],
+                    },
+                ],
+                multiplier: 2,
+                rangebits: 7,
+                x_list: vec![
+                    0, 128, 12, 46, 4, 8, 16, 23, 33, 70, 2, 6, 10, 14, 19, 28, 39, 58, 90
+                ],
+            })
+        );
+
+        // X List too long
+        let input = [
+            1, 0, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 34, 100, 38, 16, 40, 128, 2, 3, 25, 0,
+            112, 128, 144, 32, 5, 0, 20, 22, 24, 58, 134, 139, 128, 128, 92, 66, 70, 129, 65, 225,
+            152, 112, 78, 58, 109, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ];
+        let mut cursor = Cursor::new(input);
+        let mut reader = BitReader::endian(&mut cursor, LittleEndian);
+        let err = Floor::decode(&mut reader).unwrap_err();
+        assert!(matches!(err, FloorError::XListTooLong(67)));
+
+        // IOError
+        let input = [];
+        let mut cursor = Cursor::new(input);
+        let mut reader = BitReader::endian(&mut cursor, LittleEndian);
+        let err = Floor::decode(&mut reader).unwrap_err();
+        match err {
+            FloorError::IOError(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => (),
+            x => panic!("Unexpected result: {:?}", x),
+        }
+    }
 }
